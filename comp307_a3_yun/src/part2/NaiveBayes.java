2d51469c0a3f491dc511d76dcdd0f7eb3cb7cdba
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-14 18:25:26 +0800

    implement the attr_prob e.g. P(att1,...,att12=true)

--- comp307_a3_yun/src/part2/NaiveBayes.java
+++ comp307_a3_yun/src/part2/NaiveBayes.java
@@ -1,5 +1,7 @@
 package part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +12,12 @@ public class NaiveBayes {
     private List<Email> test_unlabelledEmailList;
 
     /**
-     * map classLabel to the prior probability
+     * map classLabel to the prior probability. 1 for true, 0 for false.
+     * 
      * <p>
      * i.e P(Class=spam) = ? && P(Class=non-spam) = ?
+     * <p>
+     * p(attribute:Viagra) =
      */
     private Map<String, Double> label_prior_prob_map;
 
@@ -56,31 +61,70 @@ public class NaiveBayes {
      * @return the map that map the classLabel to the corresponding probability
      */
     private Map<String, Double> calculatePriorProbability(List<Email> labelledEmails) {
-        // calculate p(class) from train_dataSet first
+        // calculate p(class) from train_dataSet
+        // as well as p(attribute 1....attribute 12)from the train_dataset
         double spamOccurance = 0.0;
         double nonSpamOccurance = 0.0;
+
+        // list for assigning attribute occurance
+        // among all the trianing labelled email data set
+        // only increase the index iff the att_label is equal to 1
+        List<Double> attributeOccuranceList = new ArrayList<Double>() {
+            {
+                for (int i = 0; i < 12; i++)
+                    add(0.0);
+            }
+        };
+
+        // map for attribute occurance
+        // Map<String, V>
+        Map<String, Double> tempMap = new HashMap<String, Double>();
         for (Email email : labelledEmails) {
             int classLabel = email.getClassLabel_spam_or_notSpam();
             if (classLabel == 1) {
                 spamOccurance++;
-            } else {
+            } else if (classLabel == 0) {
                 nonSpamOccurance++;
             }
+
+            // then calculate the corresponding attribute occurances
+            for (int attIndex = 0; attIndex < email.getAttributeList().size(); attIndex++) {
+                int att_label = email.getAttributeList().get(attIndex);
+                if (att_label == 1) {
+                    // check if it is null
+                    double oldVal = attributeOccuranceList.get(attIndex) == null ? 0
+                            : attributeOccuranceList.get(attIndex);
+                    attributeOccuranceList.set(attIndex, (oldVal + 1));
+
+                } else if (att_label == 0) {
+                    // do nothing
+                }
+
+            }
         }
-        Map<String, Double> tempMap = new HashMap<String, Double>();
-    
+
+        // calculte & assign the p(att)
+        assert attributeOccuranceList.size() == 12;
+        for (int attIndex = 0; attIndex < attributeOccuranceList.size(); attIndex++) {
+            double prob = attributeOccuranceList.get(attIndex) / train_labelledEmailList.size();
+            String key = "att" + attIndex;
+            tempMap.put(key, prob);
+            System.out.println(key + "\t" + prob);
+        }
+
+        // assigning the spam class label: p(class)
         double spamProb = spamOccurance / train_labelledEmailList.size();
         double noSpamProb = nonSpamOccurance / train_labelledEmailList.size();
-    
-        assert spamProb + noSpamProb == 1;
-    
+        System.out.println(spamProb + "\t" + noSpamProb);
+        assert spamProb + noSpamProb == 1;// assertion check
+
         tempMap.put("spam", spamProb);
         tempMap.put("noSpam", noSpamProb);
         return tempMap;
-    
+
         // this.label_prob_map.put("spam", spamProb);
         // this.label_prob_map.put("noSpam", noSpamProb);
-    
+
     }
 
 }
