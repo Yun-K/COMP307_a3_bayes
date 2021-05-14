38fb2ed5b1701cddd9dbcd50b572b7ee97df22c2
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-14 17:47:38 +0800

    optimize & implement the method for getting prior

--- comp307_a3_yun/src/part2/NaiveBayes.java
+++ comp307_a3_yun/src/part2/NaiveBayes.java
@@ -10,11 +10,11 @@ public class NaiveBayes {
     private List<Email> test_unlabelledEmailList;
 
     /**
-     * map classLabel to the probability
+     * map classLabel to the prior probability
      * <p>
      * i.e P(Class=spam) = ? && P(Class=non-spam) = ?
      */
-    private Map<String, Double> label_prob_map;
+    private Map<String, Double> label_prior_prob_map;
 
     /**
      * A constructor. It construct a new instance of NaiveBayes.
@@ -27,23 +27,8 @@ public class NaiveBayes {
         this.train_labelledEmailList = Util.readFile(labelledFilePath);
         this.test_unlabelledEmailList = Util.readFile(unLabelledFilePath);
 
-        // calculate p(class) first
-        this.label_prob_map = new HashMap<String, Double>();
-        double spamOccurance = 0.0;
-        double nonSpamOccurance = 0.0;
-        for (Email email : train_labelledEmailList) {
-            int classLabel = email.getClassLabel_spam_or_notSpam();
-            if (classLabel == 1) {
-                spamOccurance++;
-            } else {
-                nonSpamOccurance++;
-            }
-        }
-
-        double spamProb = spamOccurance / train_labelledEmailList.size();
-        double noSpamProb = nonSpamOccurance / train_labelledEmailList.size();
-        this.label_prob_map.put("spam", spamProb);
-        this.label_prob_map.put("noSpam", noSpamProb);
+        // calculate and assign the prior prob from train_dataset
+        this.label_prior_prob_map = calculatePriorProbability(this.train_labelledEmailList);
 
     }
 
@@ -57,6 +42,45 @@ public class NaiveBayes {
 
     public void test_applyClassifier() {
 
+    }
+
+    /**
+     * Description: <br/>
+     * Method for calculating the prior probability.(i.e. p(class))
+     * <p>
+     * i.e P(Class=spam) = ? && P(Class=non-spam) = ?
+     * 
+     * @author Yun Zhou
+     * @param labelledEmails
+     *            the labelled data set for training
+     * @return the map that map the classLabel to the corresponding probability
+     */
+    private Map<String, Double> calculatePriorProbability(List<Email> labelledEmails) {
+        // calculate p(class) from train_dataSet first
+        double spamOccurance = 0.0;
+        double nonSpamOccurance = 0.0;
+        for (Email email : labelledEmails) {
+            int classLabel = email.getClassLabel_spam_or_notSpam();
+            if (classLabel == 1) {
+                spamOccurance++;
+            } else {
+                nonSpamOccurance++;
+            }
+        }
+        Map<String, Double> tempMap = new HashMap<String, Double>();
+    
+        double spamProb = spamOccurance / train_labelledEmailList.size();
+        double noSpamProb = nonSpamOccurance / train_labelledEmailList.size();
+    
+        assert spamProb + noSpamProb == 1;
+    
+        tempMap.put("spam", spamProb);
+        tempMap.put("noSpam", noSpamProb);
+        return tempMap;
+    
+        // this.label_prob_map.put("spam", spamProb);
+        // this.label_prob_map.put("noSpam", noSpamProb);
+    
     }
 
 }
